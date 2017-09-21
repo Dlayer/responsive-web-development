27ed8711d241ee766bb6536a979a1782c455e5ca
Dean Blackborough <dean@g3d-development.com>
2017-09-21 13:04:27 +0100

    Links
    * View helper links

--- module/Helpers/src/Controller/DemoController.php
+++ module/Helpers/src/Controller/DemoController.php
@@ -12,8 +12,45 @@ use Zend\View\Model\ViewModel;
 
 class DemoController extends AbstractActionController
 {
-    public function navbarAction()
+    /**
+     * Layout variables for the navbar
+     *
+     * @return array
+     */
+    private function navbarVariables() : array
     {
+        return [
+            [ 'uri' => '/', 'name' => 'Home'],
+            [ 'uri' => '/content', 'name' => 'Content Manager'],
+            [ 'uri' => '/helpers', 'name' => 'View helpers', 'active' => true ]
+        ];
+    }
+
+    /**
+     * Layout variable for the header jumbotron
+     *
+     * @return array
+     */
+    private function jumbotronVariables() : array
+    {
+        return [
+            'title' => 'ZF3 view helpers',
+            'sub_title' => 'with a Bootstrap bias',
+            'tag_line' => 'View helpers I have developed for use in my projects',
+            'show_view_helper_links' => true,
+            'navbar' => $this->navbarVariables()
+        ];
+    }
+
+    /**
+     * Navbar example
+     *
+     * @return ViewModel
+     */
+    public function navbarAction() : ViewModel
+    {
+        $this->layout()->setVariables($this->jumbotronVariables());
+
         return new ViewModel();
     }
 }
--- module/Helpers/src/Controller/IndexController.php
+++ module/Helpers/src/Controller/IndexController.php
@@ -12,25 +12,45 @@ use Zend\View\Model\ViewModel;
 
 class IndexController extends AbstractActionController
 {
-    public function indexAction()
-    {
-        $this->layout()->setVariables([
-            'title' => 'View helpers',
-            'sub_title' => 'Zend Framework 3 view helpers',
-            'tag_line' => 'View helpers I developed for use in my projects',
-            'show_view_helper_links' => true,
-            'navbar' => $this->navbarVariables()
-        ]);
-
-        return new ViewModel();
-    }
-
-    private function navbarVariables()
+    /**
+     * Layout variables for the navbar
+     *
+     * @return array
+     */
+    private function navbarVariables() : array
     {
         return [
             [ 'uri' => '/', 'name' => 'Home'],
             [ 'uri' => '/content', 'name' => 'Content Manager'],
             [ 'uri' => '/helpers', 'name' => 'View helpers', 'active' => true ]
         ];
+    }
+
+    /**
+     * Layout variable for the header jumbotron
+     *
+     * @return array
+     */
+    private function jumbotronVariables() : array
+    {
+        return [
+            'title' => 'ZF3 view helpers',
+            'sub_title' => 'with a Bootstrap bias',
+            'tag_line' => 'View helpers I have developed for use in my projects',
+            'show_view_helper_links' => true,
+            'navbar' => $this->navbarVariables()
+        ];
+    }
+
+    /**
+     * Base view for module
+     *
+     * @return \Zend\View\Model\ViewModel
+     */
+    public function indexAction() : ViewModel
+    {
+        $this->layout()->setVariables($this->jumbotronVariables());
+
+        return new ViewModel();
     }
 }
