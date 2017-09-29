c6fc350ca87e7bcbb18bc649d2cde3c986d29c96
Dean Blackborough <dean@g3d-development.com>
2017-09-29 13:28:36 +0100

    Names
    * Updated names for classes, added a couple of new ones.

--- module/Content/src/Model/Tool/Page.php
+++ module/Content/src/Model/Tool/Page.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
 
 namespace Content\Model\Tool;
 
-class PageStructure
+class Page
 {
 
 }
---
+++ module/Content/src/Model/Toolbar.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Dean Blackborough <dean@g3d-development.com>
+ * @copyright Copyright (c) 2017 G3D Development Limited
+ * @license https://github.com/Dlayer/responsive-web-development/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Content\Model\Tool;
+
+class Toolbar
+{
+
+}
